d92f00e886b422e12ad9e69055de1e0aecbe6ef8
praveen <praveenkumar.shanmugam@amd.com>
2026-03-19 16:52:56 -0700

    Initial commit to open source

---
+++ api/v1beta1/preflightvalidation_types.go
@@ -0,0 +1,143 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	"strings"
+
+	"github.com/kubernetes-sigs/kernel-module-management/api/v1beta2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/conversion"
+)
+
+const (
+	VerificationTrue          = v1beta2.VerificationTrue
+	VerificationFalse         = v1beta2.VerificationFalse
+	VerificationStageImage    = v1beta2.VerificationStageImage
+	VerificationStageBuild    = v1beta2.VerificationStageBuild
+	VerificationStageSign     = v1beta2.VerificationStageSign
+	VerificationStageRequeued = v1beta2.VerificationStageRequeued
+	VerificationStageDone     = v1beta2.VerificationStageDone
+)
+
+// PreflightValidationSpec describes the desired state of the resource, such as the kernel version
+// that Module CRs need to be verified against as well as the debug configuration of the logs
+// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status
+// +kubebuilder:validation:Required
+// +kubebuilder:object:generate=false
+type PreflightValidationSpec = v1beta2.PreflightValidationSpec
+
+// +kubebuilder:object:generate=false
+type CRStatus = v1beta2.CRBaseStatus
+
+// PreflightValidationStatus is the most recently observed status of the PreflightValidation.
+// It is populated by the system and is read-only.
+// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status
+type PreflightValidationStatus struct {
+	// CRStatuses contain observations about each Module's preflight upgradability validation
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	// +optional
+	CRStatuses map[string]*CRStatus `json:"crStatuses,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// PreflightValidation initiates a preflight validations for all Modules on the current Kubernetes cluster.
+// +kubebuilder:resource:path=preflightvalidations,scope=Cluster,shortName=pfv
+// +kubebuilder:deprecatedversion
+// +operator-sdk:csv:customresourcedefinitions:displayName="Preflight Validation"
+type PreflightValidation struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	// +kubebuilder:validation:Required
+
+	Spec   PreflightValidationSpec   `json:"spec,omitempty"`
+	Status PreflightValidationStatus `json:"status,omitempty"`
+}
+
+func (p *PreflightValidation) ConvertTo(dstRaw conversion.Hub) error {
+	dst := dstRaw.(*v1beta2.PreflightValidation)
+
+	dst.ObjectMeta = p.ObjectMeta
+	dst.Spec = p.Spec
+
+	dst.Status = v1beta2.PreflightValidationStatus{}
+
+	if count := len(p.Status.CRStatuses); count > 0 {
+		dst.Status.Modules = make([]v1beta2.PreflightValidationModuleStatus, 0, count)
+
+		for k, v := range p.Status.CRStatuses {
+			namespace, name, ok := strings.Cut(k, "/")
+
+			if !ok || v == nil {
+				// Elements whose key is not a namespace name or that are nil are invalid.
+				return nil
+			}
+
+			status := v1beta2.PreflightValidationModuleStatus{
+				CRBaseStatus: *v,
+				Namespace:    namespace,
+				Name:         name,
+			}
+
+			dst.Status.Modules = append(dst.Status.Modules, status)
+		}
+	}
+
+	return nil
+}
+
+func (p *PreflightValidation) ConvertFrom(srcRaw conversion.Hub) error {
+	src := srcRaw.(*v1beta2.PreflightValidation)
+
+	p.ObjectMeta = src.ObjectMeta
+	p.Spec = src.Spec
+
+	p.Status = PreflightValidationStatus{}
+
+	if count := len(src.Status.Modules); count > 0 {
+		p.Status.CRStatuses = make(map[string]*CRStatus, count)
+
+		for _, v := range src.Status.Modules {
+			v := v
+			p.Status.CRStatuses[v.Namespace+"/"+v.Name] = &v.CRBaseStatus
+
+			// This may lead to collisions, but at least we preserve backwards compatibility.
+			p.Status.CRStatuses[v.Name] = &v.CRBaseStatus
+		}
+	}
+
+	return nil
+}
+
+// +kubebuilder:object:root=true
+
+// PreflightValidationList is a list of PreflightValidation objects.
+type PreflightValidationList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	// List of PreflightValidation. More info:
+	// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md
+	Items []PreflightValidation `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&PreflightValidation{}, &PreflightValidationList{})
+}
---
+++ cmd/manager-hub/main.go
@@ -0,0 +1,181 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"flag"
+	"time"
+
+	"github.com/kubernetes-sigs/kernel-module-management/internal/config"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/controllers"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/klog/v2/textlogger"
+	clusterv1 "open-cluster-management.io/api/cluster/v1"
+	workv1 "open-cluster-management.io/api/work/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	"github.com/kubernetes-sigs/kernel-module-management/api-hub/v1beta1"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/build"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/build/pod"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/cache"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/cluster"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/cmd"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/constants"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/controllers/hub"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/filter"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/manifestwork"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/metrics"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/module"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/nmc"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/registry"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/sign"
+	signpod "github.com/kubernetes-sigs/kernel-module-management/internal/sign/pod"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/statusupdater"
+	"github.com/kubernetes-sigs/kernel-module-management/internal/utils"
+	//+kubebuilder:scaffold:imports
+)
+
+var scheme = runtime.NewScheme()
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(v1beta1.AddToScheme(scheme))
+	utilruntime.Must(clusterv1.Install(scheme))
+	utilruntime.Must(workv1.Install(scheme))
+	//+kubebuilder:scaffold:scheme
+}
+
+func main() {
+	logConfig := textlogger.NewConfig()
+	logConfig.AddFlags(flag.CommandLine)
+
+	var configFile string
+
+	flag.StringVar(&configFile, "config", "", "The path to the configuration file.")
+
+	flag.Parse()
+
+	logger := textlogger.NewLogger(logConfig).WithName("kmm-hub")
+
+	ctrl.SetLogger(logger)
+
+	setupLogger := logger.WithName("setup")
+
+	commit, err := cmd.GitCommit()
+	if err != nil {
+		setupLogger.Error(err, "Could not get the git commit; using <undefined>")
+		commit = "<undefined>"
+	}
+
+	setupLogger.Info("Creating manager", "git commit", commit)
+
+	cfg, err := config.ParseFile(configFile)
+	if err != nil {
+		cmd.FatalError(setupLogger, err, "could not parse the configuration file", "path", configFile)
+	}
+
+	options := cfg.ManagerOptions()
+	options.Scheme = scheme
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), *options)
+	if err != nil {
+		cmd.FatalError(setupLogger, err, "unable to create manager")
+	}
+
+	client := mgr.GetClient()
+
+	nmcHelper := nmc.NewHelper(client)
+	filterAPI := filter.New(client, nmcHelper)
+
+	metricsAPI := metrics.New()
+	metricsAPI.Register()
+
+	registryAPI := registry.NewRegistry()
+	podHelperAPI := utils.NewPodHelper(client)
+	buildHelper := build.NewHelper()
+
+	buildAPI := pod.NewBuildManager(
+		client,
+		pod.NewMaker(client, buildHelper, podHelperAPI, scheme),
+		podHelperAPI,
+		registryAPI,
+	)
+
+	signAPI := signpod.NewSignPodManager(
+		client,
+		signpod.NewSigner(client, scheme, podHelperAPI),
+		podHelperAPI,
+		registryAPI,
+	)
+
+	kernelAPI := module.NewKernelMapper(buildHelper, sign.NewSignerHelper())
+
+	ctrlLogger := setupLogger.WithValues("name", hub.ManagedClusterModuleReconcilerName)
+	ctrlLogger.Info("Adding controller")
+
+	operatorNamespace := cmd.GetEnvOrFatalError(constants.OperatorNamespaceEnvVar, setupLogger)
+
+	cache := cache.New[string](10 * time.Minute)
+	ctx := ctrl.SetupSignalHandler()
+	cache.StartCollecting(ctx, 10*time.Minute)
+
+	mcmr := hub.NewManagedClusterModuleReconciler(
+		client,
+		manifestwork.NewCreator(client, scheme, kernelAPI, registryAPI, cache, operatorNamespace),
+		cluster.NewClusterAPI(client, kernelAPI, buildAPI, signAPI, operatorNamespace),
+		statusupdater.NewManagedClusterModuleStatusUpdater(client),
+		filterAPI,
+	)
+
+	if err = mcmr.SetupWithManager(mgr); err != nil {
+		cmd.FatalError(ctrlLogger, err, "unable to create controller")
+	}
+
+	eventRecorder := mgr.GetEventRecorderFor("kmm-hub")
+	jobEventReconcilerHelper := controllers.NewJobEventReconcilerHelper(client)
+
+	if err = controllers.NewBuildSignEventsReconciler(client, jobEventReconcilerHelper, eventRecorder).SetupWithManager(mgr); err != nil {
+		cmd.FatalError(setupLogger, err, "unable to create controller", "name", controllers.BuildSignEventsReconcilerName)
+	}
+
+	if err = controllers.NewJobGCReconciler(client, cfg.Job.GCDelay).SetupWithManager(mgr); err != nil {
+		cmd.FatalError(setupLogger, err, "unable to create controller", "name", controllers.JobGCReconcilerName)
+	}
+
+	//+kubebuilder:scaffold:builder
+
+	if err = mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		cmd.FatalError(setupLogger, err, "unable to set up health check")
+	}
+	if err = mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		cmd.FatalError(setupLogger, err, "unable to set up ready check")
+	}
+
+	setupLogger.Info("starting manager")
+	if err = mgr.Start(ctx); err != nil {
+		cmd.FatalError(setupLogger, err, "problem running manager")
+	}
+
+	cache.WaitForTermination()
+}
