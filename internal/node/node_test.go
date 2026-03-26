a10393daf480b67e85f9e66d98627bbb08b29fe2
yansun1996 <Yan.Sun3@amd.com>
2026-03-26 01:31:37 +0000

    [Build] OpenSource KMM repo
    Signed-off-by: yansun1996 <Yan.Sun3@amd.com>

--- internal/node/node_test.go
+++ internal/node/node_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright (c) Advanced Micro Devices, Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the \"License\");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an \"AS IS\" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package node
 
 import (
@@ -81,7 +97,7 @@ var _ = Describe("GetNodesListBySelector", func() {
 	It("list failed", func() {
 		clnt.EXPECT().List(context.Background(), gomock.Any(), gomock.Any()).Return(fmt.Errorf("some error"))
 
-		nodes, err := mn.GetNodesListBySelector(context.Background(), map[string]string{})
+		nodes, err := mn.GetNodesListBySelector(context.Background(), map[string]string{}, false)
 
 		Expect(err).To(HaveOccurred())
 		Expect(nodes).To(BeNil())
@@ -113,7 +129,7 @@ var _ = Describe("GetNodesListBySelector", func() {
 				return nil
 			},
 		)
-		nodes, err := mn.GetNodesListBySelector(context.Background(), map[string]string{})
+		nodes, err := mn.GetNodesListBySelector(context.Background(), map[string]string{}, false)
 
 		Expect(err).NotTo(HaveOccurred())
 		Expect(nodes).To(Equal([]v1.Node{node2, node3}))
